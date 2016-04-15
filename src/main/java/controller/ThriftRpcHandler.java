0898421815d06b578fd2a43b66d94f7ea471d827
Fedor Valiavko <uran230@gmail.com>
2016-04-15 19:09:02 +0300

    CRUD added

--- src/main/java/controller/ThriftRpcHandler.java
+++ src/main/java/controller/ThriftRpcHandler.java
@@ -5,34 +5,60 @@ import model.DbConnection;
 import org.apache.thrift.TException;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by Fedor on 15.04.2016.
  */
 public class ThriftRpcHandler implements LocalNetsService.Iface {
 
-    private PreparedStatement getDefinitionStatement;
+    private static final String MESSAGE_CREATE = "Record added";
+    private static final String MESSAGE_DELETE = "Record deleted";
+    private static final String MESSAGE_UPDATE = "Record updated";
+
+    private Statement statement;
     private String    definition;
     private ResultSet resultSet;
-    private String getDefinitionQuery = "SELECT definition FROM local_nets WHERE name = ?;";
 
-    public ThriftRpcHandler() {
+    @Override
+    public String deleteDef(String responce) throws TException {
         Connection connection = DbConnection.connection;
         try {
-            getDefinitionStatement = connection.prepareStatement(getDefinitionQuery);
+            statement = connection.createStatement();
+            String sql = "DELETE FROM local_nets WHERE name='" + responce + "';";
+            statement.executeUpdate(sql);
+            connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return MESSAGE_DELETE;
     }
 
     @Override
-    public String findAndPost(String responce) throws TException {
+    public String updateDef(String responce, String definition)
+            throws TException {
+        Connection connection = DbConnection.connection;
         try {
-            getDefinitionStatement.setString(1, responce);
-            resultSet = getDefinitionStatement.executeQuery();
+            statement = connection.createStatement();
+            String sql = "UPDATE local_nets SET definition = '" +
+                    definition + "' WHERE name='" + responce + "';";
+            statement.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return MESSAGE_UPDATE;
+    }
+
+    @Override
+    public String retrieveDef(String responce) throws TException {
+        Connection connection = DbConnection.connection;
+        try {
+            statement = connection.createStatement();
+            String sql = "SELECT * FROM local_nets WHERE name='" + responce + "';";
+            resultSet = statement.executeQuery(sql);
             while(resultSet.next()) {
                 definition = resultSet.getString("definition");
             }
@@ -41,5 +67,21 @@ public class ThriftRpcHandler implements LocalNetsService.Iface {
             e.printStackTrace();
         }
         return definition;
+    }
+
+    @Override
+    public String createDef(String responce, String definition)
+            throws TException {
+        Connection connection = DbConnection.connection;
+        try {
+            statement = connection.createStatement();
+            String sql = "INSERT INTO local_nets( name, definition)" +
+                    " VALUES ('" + responce +"', '" + definition + "');";
+            statement.executeUpdate(sql);
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return MESSAGE_CREATE;
     }
 }
