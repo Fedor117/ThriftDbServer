1c805775a8e8e3604d582a04eab652c7e65e8226
Dmitry Solodukha <d-mka.solod@mail.ru>
2016-04-15 18:03:35 +0300

    Replaced statement with prepared statement

--- src/main/java/controller/ThriftRpcHandler.java
+++ src/main/java/controller/ThriftRpcHandler.java
@@ -5,25 +5,34 @@ import model.DbConnection;
 import org.apache.thrift.TException;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 /**
  * Created by Fedor on 15.04.2016.
  */
 public class ThriftRpcHandler implements LocalNetsService.Iface {
 
-    private Statement statement;
+    private PreparedStatement getDefinitionStatement;
     private String    definition;
     private ResultSet resultSet;
+    private String getDefinitionQuery = "SELECT definition FROM local_nets WHERE name = ?;";
+
+    public ThriftRpcHandler() {
+        Connection connection = DbConnection.connection;
+        try {
+            getDefinitionStatement = connection.prepareStatement(getDefinitionQuery);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public String findAndPost(String responce) throws TException {
-        Connection connection = DbConnection.connection;
         try {
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery(responce);
+            getDefinitionStatement.setString(1, responce);
+            resultSet = getDefinitionStatement.executeQuery();
             while(resultSet.next()) {
                 definition = resultSet.getString("definition");
             }
